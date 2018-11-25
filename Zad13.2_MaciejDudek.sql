00e4eba892862b598b6ae2a93bf59c27148244d4
Maciej Dudek <maciejdudek96@wp.pl>
2018-11-25 17:04:29 +0100

    Add solution for 3th task
    As two separate queries.

--- Zad13.2_MaciejDudek.sql
+++ Zad13.2_MaciejDudek.sql
@@ -106,6 +106,15 @@ SELECT k.tytul, COUNT(e.e_id) AS ilosc_egzemplarzy, a.imie, a.nazwisko
     
 -- Zad.3 Ile jest tytulow i egzemplarzy ksiazek z kazdej tematyki?
 -- (dwa osobne zapytania, czy jedno?)
+-- ile tytulow
+SELECT tematyka, COUNT(tytul) FROM Ksiazka
+    GROUP BY tematyka;
+-- ile egzemplarzy
+SELECT k.tematyka, COUNT(e.sygnatura)
+    FROM Ksiazka k 
+        JOIN Egzemplarz e ON k.k_id=e.e_id
+    GROUP BY k.tematyka;
+-- jako jedno?
 
 -- Zad.4 Dla kazdego ISBN wyswietl najmniejsza sygnature. Posortuj wg. sygnatur.
 SELECT ISBN, MIN(sygnatura) FROM Egzemplarz
